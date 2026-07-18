91fabea5cbae5ee912c298582f3c6ffe731c99cd
S7S <193077675+S7S37@users.noreply.github.com>
2026-07-19 02:09:51 +0300

    added sketching functionality with random color

--- javascript.js
+++ javascript.js
@@ -1,11 +1,16 @@
 const container = document.querySelector(".container")
 
-/*make a grid using flexbox*/
-for (let i = 0;i<16 * 16;i++){
+// make a grid using flexbox
+for (let i = 0;i<100*100;i++){
     const div = document.createElement("div");
-    let boxSize = 960/16
+    let boxSize = 960/100
     div.style.flexBasis = `${boxSize}px`
     div.style.height = `${boxSize}px`
     div.classList.add("pixel-Box")
     container.appendChild(div)
+// setting a hovering effect
+    div.addEventListener("mouseenter", (e)=> {
+        div.style.backgroundColor = `rgb(${Math.random() * 255},${Math.random()* 255},${Math.random()* 255})`
+    })
 }
+
